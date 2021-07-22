c39e3f4bd443571701b258c24db8968db397460f
vikasmsit <72131953+vikasmsit@users.noreply.github.com>
2021-07-22 12:27:10 +0530

    Catalog with improvements

--- storage.js
+++ storage.js
@@ -1,13 +1,54 @@
 // Set starting value of counter to 0
+
 if (!localStorage.getItem('prodList')){
     localStorage.setItem('prodList',JSON.stringify([]));
+}
+
+function renderHTML(data){
+    var catList=document.getElementById("catlist")
+    var htmlString=``
+
+    console.log(data)
+    console.log(data.length)
+    for (var i=0;i<data.length;i++){
+        var image=data[i].image
+
+        var temp_img=image.split("\\")
+
+        temp_img=temp_img[temp_img.length-1]
+
+        console.log(temp_img)
+
+        let temp=`
+        <div class="card" style="width: 18rem;">
+        <img src=${temp_img} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Name</h5>
+          <p class="card-text">${data[i].name}</p>
+          <h5 class="card-title">Description</h5>
+          <p class="card-text">${data[i].desc}</p>
+          <h5 class="card-title">Price</h5>
+          <p class="card-text">${data[i].Price}</p>
+          <h5 class="card-title">MFD</h5>
+          <p class="card-text">${data[i].mfd}</p>
+          <h5 class="card-title">Expiry Date</h5>
+          <p class="card-text">${data[i].exp}</p>
+        </div>
+      </div>
+      `
+      htmlString=htmlString+temp
+    }
+
+    catList.insertAdjacentHTML('beforeend',htmlString)
+
 }
 console.log(JSON.parse(localStorage.getItem('prodList')))
 prodList=JSON.stringify(localStorage.getItem('prodList'))
 // Load current value of  counter
 document.addEventListener('DOMContentLoaded', () => {
-document.querySelector('#counter').innerHTML = prodList
 
+let prodList=JSON.parse(localStorage.getItem('prodList'))
+renderHTML(prodList)
 // Count every time button is clicked
 document.querySelector('button').onclick = () => {
     // Increment current counter
@@ -30,7 +71,10 @@ document.querySelector('button').onclick = () => {
 
     prodList.push(new_prod)
     // Update counter
-    document.querySelector('#counter').innerHTML = prodList.toString();
+    //document.querySelector('#counter').innerHTML = prodList.toString();
+    //renderHTML(prodList)
+    //renderHTML(new_prod)
+    console.log("hello "+ prodList.length)
     localStorage.setItem('prodList', JSON.stringify(prodList));
 }
 });
